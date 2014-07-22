55e5f5ea449721799091abf985657aa95a41ee3d
JulieL417 <long53@email.franklin.edu>
2014-07-22 15:08:18 -0400

    Create Resume.php

---
+++ Resume.php
@@ -0,0 +1,104 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta name="author" content="">
+	<link rel="stylesheet" type="text/css" href="css/style.css" media="screen">
+<title>Resume</title>
+</head>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+</body>
+</html>	<body>
+		<div id="wrapper">
+			<center><img src="images/banner2.jpg"></center>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<div id="nav" align="center">
+
+	<a href="index.php"> Home </a>
+	<a href="about.php"> About </a>
+	<a href="portfolio.php"> Portfolio </a>
+	<a href="education.php"> Education </a>
+	<a href="resume.php"> Resume </a>
+	<a href="contact.php"> Contact </a>
+
+</div> <!-- end #nav -->
+</body>
+</html>          <div id="content">
+<h3> <u>Resume</u> </h3>
+                <p align="center">Julie Renee Long</p>
+                <p align="center">JulieL417@yahoo.com</p>
+                <p align="center">Cell Phone: 330-696-9653</p>
+				<p align="center">Home Phone: 330-753-1472</p>
+                <br />
+                <p><b style="font-size:18px">Experience:</b></p>
+                <p><b style="font-size:14px">Quikey Manufacturing Co. - Akron, Ohio</b></p>
+                <p><em style="font-size:14px">Web Developer - September 2011 to Present</em></p>
+                <p style="font-size:14px">Tasks: Maintain and update the data on six different websites.</p>
+                <p style="font-size:14px">Use mostly HTML and CSS as the source code for the websites.</p>
+                <br />
+                <p><b style="font-size:14px">Western Reserve Controls (WRC) - Akron, Ohio</b></p>
+                <p><em style="font-size:14px">Production Worker - June 2008 to August 2011</em></p>
+                <p style="font-size:14px">Tasks: Hand soldering circuit boards and modules.</p>
+                <p style="font-size:14px">	Testing and cleaning the substraints.</p>
+                <br />
+                <p><b style="font-size:18px">Education:</b></p>
+                <p title="college"><b style="font-size:14px">Franklin University - Columbus, Ohio</b></p>
+                <p style="font-size:14px">Bachelor's of Science, Web Development - Summer 2014 (Expected)</p>
+                <p style="font-size:14px">Skills: PHP, Java, JavaScript, HTML, CSS, MySQL, JQuery, APIs, AJAX.</p>
+                <br />
+                <p title="college"><b style="font-size:14px">Stark State College of Technology - Canton, Ohio</b></p>
+                <p style="font-size:14px">Associates of Applied Business, E-Commerce Technology - Spring 2008 (Graduated)</p>
+                <blockquote>
+                <p style="font-size:14px">	- Phi Theta Kappa Honor Society</p>
+                </blockquote>
+                <p style="font-size:14px">Skills: Java, PHP, JavaScript, ASP.NET, HTML, CSS, Flash, Photoshop, Dreamweaver, XHTML.</p>
+                <br />
+				<p style="font-size:14px" align="center">If you would like to a copy of my resume, please <a href="Resume.pdf">Click Here</a> to download it.</p>
+				<br />
+                <p align="center">References Available Upon Request</p>
+  
+          </div> <!-- end #content -->
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+</body>
+</html><div id="footer" align="center">
+
+	<p> &copy 2014 - <a href="mailto:JulieL417@yahoo.com">Julie Long</a> - All Rights Reserved </p>
+
+</div> <!-- end #footer -->
+</body>
+</html>
+		</div> <!-- End #wrapper -->
+</body>
+</html>
