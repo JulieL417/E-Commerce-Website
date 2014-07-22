3195a0f9f50221153ded17bd06a08fb1ddc611de
JulieL417 <long53@email.franklin.edu>
2014-07-22 15:09:19 -0400

    Create about.php

---
+++ about.php
@@ -0,0 +1,108 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+
+<meta charset="utf-8">
+
+<meta name="description" content="">
+
+<meta name="keywords" content="">
+
+<meta name="author" content="">
+
+<link rel="stylesheet" type="text/css" href="css/style.css" media="screen">
+
+<title>About Me</title>
+
+</head>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+</body>
+</html>
+<body>
+	<div id="wrapper">
+<center><img src="images/banner2.jpg"></center>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<div id="nav" align="center">
+
+	<a href="index.php"> Home </a>
+	<a href="about.php"> About </a>
+	<a href="portfolio.php"> Portfolio </a>
+	<a href="education.php"> Education </a>
+	<a href="resume.php"> Resume </a>
+	<a href="contact.php"> Contact </a>
+
+</div> <!-- end #nav -->
+</body>
+</html>
+
+<div id="content">
+<h3> <u>About Me</u> </h3>
+
+<p>
+  Hello, my name is Julie Long. I was born on April 17. 
+  I currently live in Barberton, OH and work full-time at Quikey Manufacturing Co., in Akron, OH as a Web Developer. 
+</p>
+<p>
+	I started working with HTML and CSS my junior year in high school. 
+	My teacher and mentor, Mrs. Begert, recognized my talent, and recommended Web Design as a career choice. 
+	After graduating, I enrolled to Stark State College in Canton, OH to follow my dream of becoming a Web Developer.
+</p>
+<p> 
+	I earned an Associates Degree in Applied Business May 2008. 
+	In the fall of 2008, I enrolled at Franklin University located in Columbus, OH for my Bachelor's of Science Degree.
+	I completed one full year. In 2013, a friend of mine convinced me to go back to Franklin, and complete my Bachelor's of Science Degree. 
+</p>
+<p>
+	I am currently on track to graduate with honors September 2014.
+</p>
+
+
+</div> <!-- end #content -->
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+</body>
+</html><div id="footer" align="center">
+
+	<p> &copy 2014 - <a href="mailto:JulieL417@yahoo.com">Julie Long</a> - All Rights Reserved </p>
+
+</div> <!-- end #footer -->
+</body>
+</html>
+
+	</div> <!-- End #wrapper -->
+</body>
+</html>
